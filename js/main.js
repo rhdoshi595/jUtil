3cbc9acca18a9cebc427bfd785329200a94ddbd3
rhdoshi595 <rhdoshi595@gmail.com>
2017-01-27 15:35:27 -0500

    Cleaned up repo and recommit

--- js/main.js
+++
@@ -1,8 +0,0 @@
-const View = require('./snake-view.js');
-const Game = require('./snake.js');
-const $l = require('./../jUtility/lib/main.js');
-
-$l(() => {
-  const rootEl = $l('.snake');
-  new View(rootEl);
-});
